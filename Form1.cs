bbad5073d92a6f7352411429fb72cc51d1ff4906
Mahdi-Rashidiyan <mahdiworkspace1384@gmail.com>
2024-10-12 12:54:36 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string APIKey = "39470ee7191c885acc3df81f5a2b4996";
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+
+        private void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", TbCity.Text, APIKey);
+                var json = web.DownloadString(url);
+                Weatherinfo.root Info = JsonConvert.DeserializeObject<Weatherinfo.root>(json);
+                pic_icon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                lab_condition.Text = Info.weather[0].main;
+                lab_detail.Text = Info.weather[0].description;
+                lab_sunset.Text = convertDateTime(Info.sys.sunset).ToString();
+                lab_sunrise.Text = convertDateTime(Info.sys.sunrise).ToString();
+                lab_windspeed.Text = Info.wind.speed.ToString();
+                lab_pressure.Text = Info.main.pressure.ToString();
+
+            }
+            DateTime convertDateTime(long millisec)
+            {
+                DateTime day = new DateTime(1970,1,1,0,0,0, DateTimeKind.Utc);
+                day = day.AddSeconds(millisec).ToLocalTime();
+                return day; 
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Weatherinfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal class Weatherinfo
+    {
+        public class coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+        public class  weather
+        {
+            public string main {  get; set; }
+            public string description {  get; set; }
+            public string icon { get; set; }
+
+        }
+        public class main
+        {
+            public double temp { get; set; }
+            public double pressure { get; set; }
+            public double humidity { get; set; }
+
+        }
+        public class wind
+        {
+            public double speed { get; set; }
+        }
+        public class sys
+        {
+            public long sunrise { get; set; }
+            public long sunset { get; set; }
+
+        }
+        public class root
+        {
+            public coord coord { get; set; }
+            public List<weather> weather { get; set; }
+            public main main { get; set; }
+            public wind wind { get; set; }
+            public sys sys {  get; set; }
+
+        }
+    }
+}
